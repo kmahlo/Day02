39ead31ba17c9ec48433975208149bc40b19c6c3
kagiso Ronald Mahlo <kmahlo@c5r6s5.wethinkcode.co.za>
2020-06-21 15:10:49 +0200

    ex01

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_reverse_alphabet.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmahlo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 10:16:02 by kmahlo            #+#    #+#             */
+/*   Updated: 2020/06/20 10:27:53 by kmahlo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet()
+
+{
+	char i;
+	i = 'z';
+	while(i >= 'a')
+	{
+		ft_putchar(i);
+			i--;
+	}
+}
+int	main()
+{
+	ft_print_reverse_alphabet();
+	return(0);
+}

5dcda3782fc9d66ffd3ce1c24ff4ad95eaee889e
kagiso Ronald Mahlo <kmahlo@c5r6s5.wethinkcode.co.za>
2020-06-21 15:14:20 +0200

    ex05

---
+++ ex05/ft_print_comb2.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_comb2()
+{
+	char a;
+	char b;
+	a = 0;
+		while(a < 99 )
+		{
+			b= a + 1;
+			while(b <= 99)
+			{
+				ft_putchar(a/10 + '0');
+				ft_putchar(a%10 + '0');
+				ft_putchar(' ');
+				ft_putchar(b/10 + '0');
+				ft_putchar(b%10 + '0');
+				ft_putchar(',');
+				ft_putchar(',');
+				b++;
+			}
+			a = a + 1;
+			}
+}
+
